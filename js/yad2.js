438d89e637bacb3c2051ca6b0543f005b151f11e
Shahar Lev <shahar@e8storage.com>
2016-08-27 01:06:50 +0300

    Have yad2 page use jQuery

--- js/yad2.js
+++ js/yad2.js
@@ -1,19 +1,3 @@
-var clearNode = function(node) {
-	while (node.firstChild) {
-	    node.removeChild(node.firstChild);
-	}
-};
-
-var appendText = function(node, text) {
-	return node.appendChild(document.createTextNode(text));
-};
-
-var appendElement = function(node, name) {
-	child = document.createElement(name);
-	node.appendChild(child);
-	return child;
-};
-
 var yad2Store = {
 	clear: function(callback) {
 		chrome.runtime.sendMessage({type: 'clear'}, function(response) {
@@ -23,43 +7,35 @@ var yad2Store = {
 	},
 	update: function() {
 		chrome.browserAction.setIcon({path: 'img/icon19.png'});
-		var queriesElem = document.getElementById('queries');
-		var divUrls = document.getElementById('divUrls');
-		clearNode(queriesElem);
-		clearNode(divUrls);
-		var tableHeaderElem = appendElement(queriesElem, 'tr');
-		appendText(appendElement(tableHeaderElem, 'th'), 'Last Inspected');
-		appendText(appendElement(tableHeaderElem, 'th'), 'URL');
+		$("#queries tr").not(":first").remove();
+		$("#divUrls").empty();
 		chrome.storage.local.get(['queries', 'urls'], function (items) {
-			var queries = items['queries'];
-			if (queries === undefined) queries = [];
-			for (var i = 0; i < queries.length; ++i) {
-				var query = queries[i];
-				var rowElem = appendElement(queriesElem, 'tr');
-				appendText(appendElement(rowElem, 'td'), query.date);
-				var linkElement = appendElement(appendElement(rowElem, 'td'), 'a');
-				linkElement.setAttribute('href', query.url);
-				linkElement.setAttribute('target', '_blank');
-				appendText(linkElement, 'Link');
-			}
-			var urls = items['urls'];
-			if (urls === undefined) urls = [];
-			for (var i = 0; i < urls.length; ++i) {
-				var url = urls[i];
-				if (i > 0)
-					appendElement(divUrls, 'br');
-				var urlElem = appendElement(divUrls, 'a');
-				urlElem.setAttribute('href', url);
-				urlElem.setAttribute('target', '_blank');
-				appendText(urlElem, url);
-			}
+			$("#queries").append($.map(items.queries, function(query) {
+				var linkElem = $('<a>')
+					.prop('href', query.url)
+					.prop('target', '_blank')
+					.text('Link');
+				return $('<tr>')
+					.append($('<td>').text(query.date))
+					.append($('<td>').append(linkElem));
+			}));
+			$("#divUrls").append($.map(items.urls, function(url) {
+				return [
+				        $('<a>')
+						.prop('href', url)
+						.prop('target', '_blank')
+						.text(url),
+					$('<br>')];
+			}));
 		});
 	}
 };
 
 yad2Store.update();
 
-document.getElementById("clearButton").addEventListener("click", function () {
+$("#refreshButton").on("click", yad2Store.update);
+
+$("#clearButton").on("click", function () {
 	var answer = window.confirm("Are you sure you want to clear all URLs?");
 	if (answer) {
 		yad2Store.clear(yad2Store.update);
