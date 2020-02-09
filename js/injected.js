0a114197425f69caea091fd58c88b62b94a6ff17
Shahar Lev <shaharl@ai21.com>
2020-02-09 12:48:14 +0200

    update injected selection

--- js/injected.js
+++ js/injected.js
@@ -4,15 +4,14 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		return;
 	}
 	if (!("extractRent" in request)) return;
-	var nodes = document.querySelectorAll('*[onclick*="NadlanID"]');
+	var nodes = document.getElementsByClassName('feed_item')
 	var urlsNow = [];
 	for (var i = 0; i < nodes.length; ++i) {
-		var onClickAttrib = nodes[i].getAttribute("onclick");
-		var groups = /([a-zA-Z]*)Details.php','NadlanID','([0-9a-f]+)'/.exec(onClickAttrib);
-		if (!groups) continue;
-		if (["tivsales", "tivrent"].indexOf(groups[1]) != -1 && !request.extractRent.realtors) continue;
-		var url = "http://www.yad2.co.il/Nadlan/" + groups[1] + "_info.php?NadlanID=" + groups[2];
-		urlsNow[urlsNow.length] = url;
+		var item_id = nodes[i].getAttribute('item-id')
+		if (!item_id || (nodes[i].classList.contains('agency') && !request.extractRent.realtors))
+			continue
+		var url = "https://www.yad2.co.il/item/" + item_id;
+		urlsNow.push(url);
 	}
 	urlsNow = Array.from(new Set(urlsNow)).sort();
 	var extractVals = {
